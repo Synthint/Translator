c931008a0c5fcbc468d5fdff54bbb8117e9befba
Sean Meyer <seanm0914@gmail.com>
2022-09-29 23:35:57 -0400

    added more commands, changed formatting of output

--- speechRecon.py
+++ speechRecon.py
@@ -6,7 +6,7 @@ import os
 
 TRANSLATE_TO_LANGUAGE = "pl"
 TRANSLATE_FROM_LANGUAGE = "en"
-LANG_DETECTION = False
+LANG_DETECTION =  False
 
 
 translator = Translator()
@@ -47,7 +47,7 @@ def main():
                 text = recognizer.recognize_google(audio, language = TRANSLATE_FROM_LANGUAGE, show_all = True)
             if len(text) > 0:
                 sentence = text['alternative'][0]['transcript']
-                print(f"Recognized:\t {sentence}")
+                print(f"    -Recognized:\t {sentence}")
                 if LANG_DETECTION:
                     print(f"Language Detected = ",translator.detect(sentence))
 
@@ -56,6 +56,10 @@ def main():
                 if TRANSLATE_TO_LANGUAGE != "en":
                     commandTranslation = translate(sentence, 'en', TRANSLATE_FROM_LANGUAGE)
                 
+                print(f" {TRANSLATE_TO_LANGUAGE} -translated:\t",translation)
+                speak(translation,TRANSLATE_TO_LANGUAGE)
+                print("- - - - - - - - - - - - - - - - - -")
+
                 if commandTranslation in COMMAND_PHRASES:
                     command = COMMAND_PHRASES[commandTranslation]
                     if command == 0:
@@ -63,12 +67,8 @@ def main():
                         break
                     else:
                         COMMAND_PHRASES[commandTranslation]()
-                else:
-                    print("translated:\t",translation)
-                    speak(translation,TRANSLATE_TO_LANGUAGE)
-                    print("- - - - - - - - - - - - - - - - - -")
+                    
 
-            recognizer = speech_recognition.Recognizer()
             continue
 
 def change_output_language():
@@ -97,13 +97,46 @@ def change_output_language():
 
     newLang = langToCode[langTrans.lower()]
     TRANSLATE_TO_LANGUAGE = newLang
-    print(f"CHANGED TO {langTrans.upper()}")
+    print(f"CHANGED TO {langTrans.upper()} - {TRANSLATE_TO_LANGUAGE}")
     print("\n==========================================\n")
 
 
+def change_input_language():
+    print("\n=== CHANGE INPUT LANGUAGE COMMAND SENT ===")
+    global TRANSLATE_FROM_LANGUAGE
+    langTrans = ""
+    langToCode = {v: k for k, v in constants.LANGUAGES.items()}
+    attempts = 0
+    while langTrans.lower() not in langToCode:
+        print("\nENTER VALID LANGUAGE")
+        with speech_recognition.Microphone() as mic:
+            recognizer.adjust_for_ambient_noise(mic, duration = 0.001)
+            audio = recognizer.listen(mic)
+            if LANG_DETECTION:
+                text = recognizer.recognize_google(audio, show_all = True)
+            else:
+                text = recognizer.recognize_google(audio, language = TRANSLATE_FROM_LANGUAGE, show_all = True)
+            if len(text) > 0:
+                sentence = text['alternative'][0]['transcript']
+                langTrans = translate(sentence, 'en', TRANSLATE_FROM_LANGUAGE)
+                print(f"Attempt to swtich language to:\t {langTrans.upper()}")
+        attempts = attempts + 1
+        if attempts > 7:
+            print("\n====NO VALID LANGUAGE DETECTED, RETURNING ====\n")
+            return
+
+    newLang = langToCode[langTrans.lower()]
+    TRANSLATE_FROM_LANGUAGE = newLang
+    print(f"CHANGED TO {langTrans.upper()} - {TRANSLATE_FROM_LANGUAGE}")
+    print("\n=========================================\n")
+
+
 COMMAND_PHRASES = {
-    "stop recognition" : 0,
-    "change output language": change_output_language
+    "stop program" : 0,
+    "shutdown program" : 0,
+    "halt program" : 0,
+    "change output language": change_output_language,
+    "change input language": change_input_language
 }
 
 main()
